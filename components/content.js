3ddb5fc0b1e0df6c4218b67d1d3d63f0ef094727
Suman Shrestha <summonshr@gmail.com>
2019-12-24 17:50:33 +0545

    WIP

---
+++ components/content.js
@@ -0,0 +1,50 @@
+var Editor = require('@tinymce/tinymce-vue').default;
+var moment = require("moment")
+let { mapState } = Vuex
+let fs = require('fs')
+
+Vue.component('note', {
+    template: fs.readFileSync('./templates/content.html', 'utf-8'),
+    methods: {
+        moment
+    },
+    components: {
+        'tinymce-editor': Editor,
+    },
+    data() {
+        return { editor: false }
+    },
+    computed: {
+        ...mapState(['notes', 'categories', 'favourites', 'selected', 'current']),
+        current: {
+            get() {
+                return this.notes.filter(e => e.key === this.selected.note)[0]
+            }
+        },
+        title: {
+            get() {
+                return this.current && this.current.title
+            },
+            set(title) {
+                this.current && store.commit('updateNote', { title })
+            }
+        },
+        description: {
+            get() {
+                return this.current && this.current.description
+            },
+            set(description) {
+                store.commit('updateNote', { description })
+            }
+        },
+        note: {
+            get() {
+                return this.current && this.current.content
+            },
+            set(content) {
+                store.commit('updateNote', { content })
+            }
+        },
+    }
+
+})
