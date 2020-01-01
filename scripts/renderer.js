4acb68eb0ff2edd6d4741a1bf0d76b1ef71b7100
Suman Shrestha <summonshr@gmail.com>
2020-01-01 19:42:01 +0545

    WIP: Parcel has been adapted.

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -3,20 +3,4 @@ import PortalVue from 'portal-vue'
 Vue.use(PortalVue)
 import App from './app.vue'
 
-// function component(name, url) {
-//     let content = fs.readFileSync(url + '.vue', 'utf-8')
-//     let element = document.createElement('body')
-//     element.innerHTML = content
-//     content = element.children[1].innerHTML.trim()
-//     return Vue.component(name, {
-//         template: element.children[0].innerHTML.trim(),
-//         ...eval('let module={};' + content)
-//     })
-// }
-
-// component('category-list', './components/category-list')
-// component('to-do', './components/to-do')
-// component('note-list', './components/note-list')
-// component('note', './components/content')
-
 new Vue({ render: createElement => createElement(App) }).$mount('#app');
